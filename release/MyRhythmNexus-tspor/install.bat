8dad135519782d9e1b33780bd2cdbb9f0c056202
Hazar Ute <hazarute@gmail.com>
2025-12-09 12:14:53 +0300

    web: add logout button to card detail; normalize phone lookup in /web/login; desktop: respect packaged backend_urls in config

--- release/MyRhythmNexus-tspor/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set APPDIR=%APPDATA%\MyRhythmNexus
-if not exist "%APPDIR%" mkdir "%APPDIR%"
-copy "%~dp0\MyRhythmNexus_v1.0.3.exe" "%APPDIR%\MyRhythmNexus_v1.0.3.exe" /Y
-copy "%~dp0\config.json" "%APPDIR%\config.json" /Y
-echo Kurulum tamamlandi. Uygulamayi baslatmak icin exe'yi calistirin.
-pause
-endlocal
--- release/MyRhythmNexus-tspor/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-APPDIR=${XDG_CONFIG_HOME:-$HOME/.config}/MyRhythmNexus
-mkdir -p "$APPDIR"
-cp "$(dirname "$0")/MyRhythmNexus_v1.0.3.exe" "$APPDIR/MyRhythmNexus_v1.0.3.exe"
-cp "$(dirname "$0")/config.json" "$APPDIR/config.json"
-echo 'Kurulum tamamlandi.'
